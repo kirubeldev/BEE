fedbd0b9fc9b1e61001bda0b305cea0d762106f5
kirubeldev <kirubelmenberu35@gmail.com>
2024-04-17 16:05:51 +0300

    mobile bar done

---
+++ src/Component/Section/Vision.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+
+const Vision = () => {
+  return (
+    <div>
+  <section className="service-one">
+  <div className="auto-container">
+    <div className="row g-0">
+      {/* Service Block One */}
+      <div className="service-block_one col-lg-4 col-md-6 col-sm-12">
+        <div className="service-block_one-inner">
+          <div className="service-block_one-upper">
+            <div className="service-block_one-icon flaticon-wifi-router-1" />
+            <h4 className="service-block_one-heading">
+              <a href="service-detail.html">
+                
+              VISION <br /> 
+              </a>
+            </h4>
+          </div>
+          <div className="service-block_one-text" style={{marginTop:"40px" , textAlign:"justify",paddingBottom:"25px"}}>
+       our Vision is to build relationships with and between black-owned businesses globally in order to create an economic Ecosystem of interconnected Black-owned businesses. BEE Channel as a platform will be a central driver of entrepreneurship and economic upward mobility. <br/><br/>
+
+          </div>
+          <a className="service-block_one-more" href="service-detail.html">
+            {/* Get Service */}
+          </a>
+        </div>
+      </div>
+      {/* Service Block One */}
+      <div className="service-block_one col-lg-4 col-md-6 col-sm-12">
+        <div className="service-block_one-inner">
+          <div className="service-block_one-upper">
+            <div className="service-block_one-icon flaticon-smartphone" />
+            <h4 className="service-block_one-heading">
+              <a href="service-detail.html">
+              MISSION
+
+              </a>
+            </h4>
+          </div>
+          <div className="service-block_one-text" style={{marginTop:"40px" , textAlign:"justify", paddingBottom:"10px"}}>
+          <p>To create a media platform that produces programming based on the following pillars of production:<br/>
+ Promoting Pan-African Upliftment <br/>
+ Commanding the Narrative of the Global Black Experienc<br/>
+ Encourage African-to-African Collaboration<br/>
+ Plant the Seeds for Black ECONOMY Excellence</p>
+
+ 
+          </div>
+          <a className="service-block_one-more" href="service-detail.html">
+            {/* Get Service */}
+          </a>
+        </div>
+      </div>
+      {/* Service Block One */}
+      <div className="service-block_one col-lg-4 col-md-6 col-sm-12">
+        <div className="service-block_one-inner">
+          <div className="service-block_one-upper">
+            <div className="service-block_one-icon flaticon-cinema" />
+            <h4 className="service-block_one-heading">
+              <a href="service-detail.html">
+              GOALS
+              </a>
+            </h4>
+          </div>
+          <div className="service-block_one-text"  style={{marginTop:"40px" , textAlign:"justify"}}>
+          To utilize the multimedia platforms to present BEE
+Channel content that will become the voice of the
+Purpose Black Nation of followers and members. In so
+doing, within 15 years the BEE Channel will work
+globally to:
+<br/> 1) ECONOMYally Impact 1 Billion Black People
+<br/> 2) Create an ECONOMY Ecosystem of interconnected
+Black-owned businesses
+<br/> 3) Impact 1 Billion Black Enterprises
+
+          </div>
+          <a className="service-block_one-more" href="service-detail.html">
+            {/* Get Service */}
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+    </div>
+  )
+}
+
+export default Vision
